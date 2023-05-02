b659c2fd1b3361e04d63d39e19350eebb48c703e
Loginov10 <120027474+Loginov10@users.noreply.github.com>
2023-05-02 13:34:30 +0300

    Update ANIMACIA.py

--- ANIMACIA.py
+++ ANIMACIA.py
@@ -1,93 +1,95 @@
 
-import pygame,sys
+import pygame
+import sys
+from random import*
 import random
-def Liik():
-	global posX,posY
-	sammX=0
-	sammY=0
-	
-	if posX==0 and posY==0:
-		sammX=1
-		sammY=0		
-	if posX==X-among.get_rect().width and posY<=Y-among.get_rect().height:	
-		sammX=0
-		sammY=1
-	if posX<=X-among.get_rect().width and posY==Y-among.get_rect().height:		
-		sammX=1
-		sammY=0
-		sammX=-sammX
-	if posX==0 and posY>=Y-among.get_rect().height:					
-		sammX=0
-		sammY=1
-		sammY=-sammY
-	
-	posX+=sammX
-	posY+=sammY
-	ekraan.blit(among,(posX,posY))	
-	pygame.display.flip()
-	ekraan.fill(hall)
+
+def Päri(): #По часовой
+    global posX, posY, sammX, sammY
+    if posX == X - pilt.get_rect().width and posY == Y - pilt.get_rect().height:
+        sammX, sammY = -1, 0
+    elif posX == X - pilt.get_rect().width and posY == 0:
+        sammX, sammY = 0, 1
+    elif posX == 0 and posY == 0:
+        sammX, sammY = 1, 0
+    elif posX == 0 and posY == Y - pilt.get_rect().height:
+        sammX, sammY = 0, -1
+    posX += sammX
+    posY += sammY
+    ekraan.blit(pilt, (posX, posY))
+    pygame.display.flip()
+    pygame.time.delay(10)
 
 
-def Likk2():
-	global posX,posY
-	sammX=0
-	sammY=0
-	
-	if posX==0 and posY==0:
-		sammX=1
-		sammY=0		
-	if posX==X-among.get_rect().width and posY<=Y-among.get_rect().height:	
-		sammX=0
-		sammY=1
-	if posX<=X-among.get_rect().width and posY==Y-among.get_rect().height:		
-		sammX=1
-		sammY=0
-		sammX=-sammX
-	if posX==0 and posY>=Y-among.get_rect().height:					
-		sammX=0
-		sammY=1
-		sammY=-sammY
-		 
-	posX+=sammX
-	posY+=sammY
-	ekraan.blit(among,(posX,posY))	
-	pygame.display.flip()
-	ekraan.fill(roheline)
+def Vastu(): #Против часовой
+    global posX, posY, sammX, sammY
+    if posY <= 0 and posX >= ekraan.get_width() - pilt.get_width():
+        sammX = -2
+        sammY = 0
+    elif posY >= ekraan.get_height() - pilt.get_height() and posX >= ekraan.get_width() - pilt.get_width():
+        sammX = 0
+        sammY = -2
+    elif posY >= ekraan.get_height() - pilt.get_height() and posX <= 0:
+        sammX = 2
+        sammY = 0
+    elif posY <= 0 and posX <= 0:
+        sammX = 0
+        sammY = 2
+    posX += sammX
+    posY += sammY
+    ekraan.blit(pilt, (posX, posY))
+    pygame.display.flip()
+    pygame.time.delay(10)
+
+
+def Randm():#Оталкивается от кроёв
+    global posX, posY, sammX, sammY
+    if posY <= 0:
+        sammX = random.choice([-2, -1, 1, 2])
+        sammY = random.choice([1, 2])
+    elif posY >= ekraan.get_height() - pilt.get_height():
+        sammX = random.choice([-2, -1, 1, 2])
+        sammY = random.choice([-1, -2])
+    elif posX >= ekraan.get_width() - pilt.get_width():
+        sammX = random.choice([-1, -2])
+        sammY = random.choice([-2, -1, 1, 2])
+    elif posX <= 0:
+        sammX = random.choice([1, 2])
+        sammY = random.choice([-2, -1, 1, 2])
+    posX += sammX
+    posY += sammY
+    ekraan.blit(pilt, (posX, posY))
+    pygame.display.flip()
+    pygame.time.delay(10)
+
+
 pygame.init()
-#värvid
-koolane=[255,255,10]
-punane=[255,0,0]
-hall=[200,200,200]
-roosa=[255,150,255]
-roheline=[100,255,100]
+X, Y = 640, 480
+ekraan = pygame.display.set_mode([X, Y])
+pygame.display.set_caption("raketa")
+# цвет фона
+fon = (100, 255, 100)
+ekraan.fill(fon)
 
-#ekraani suurus
-X=640
-Y=480
-ekraan=pygame.display.set_mode([X,Y])
-pygame.display.set_caption("Animatsion")
-ekraan.fill(roheline)
+
+pilt=pygame.image.load("among.png")
+posX, posY = X - pilt.get_rect().width, Y - pilt.get_rect().height
+sammX, sammY = 0, 0
+
+lõpp=False
+
 kell=pygame.time.Clock()
-among=pygame.image.load("among.png")
-
-posX=X-among.get_rect().width
-posY=Y-among.get_rect().height
 
 
-
-lopp=False
-r=random.randint(1,2)
-while not lopp:
-	kell.tick(200)
-	events=pygame.event.get()
-	
-	for i in pygame.event.get():
-		if i.type==pygame.QUIT():
-			sys.exit()
-	if r==1:
-		Liik()
-		
-	if r==2:
-		Likk2()
-		
+lõpp=False
+kell=pygame.time.Clock()
+v=[Päri,Vastu,Randm]
+while not lõpp:
+    kell.tick(400)
+    events = pygame.event.get()
+    for i in pygame.event.get():       
+        if i.type == pygame.QUIT:
+            lõpp = True
+    random.choice(v)()
+    ekraan.fill(fon)
 pygame.quit()
